9c1224029744abe714a09c414daea8b81d7e5fbe
datleeey <datleeey@gmail.com>
2024-10-24 16:50:41 +0300

    Добавьте файлы проекта.

---
+++ laba2-27/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace laba2_27
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Клас для опису вектора в тривимірному просторі
+        public class Vector3D
+        {
+            // Закриті поля для координат вектора
+            private double x, y, z;
+
+            // Властивості для доступу до координат
+            public double X
+            {
+                get { return x; }
+                set { x = value; }
+            }
+
+            public double Y
+            {
+                get { return y; }
+                set { y = value; }
+            }
+
+            public double Z
+            {
+                get { return z; }
+                set { z = value; }
+            }
+
+            // Конструктор без параметрів
+            public Vector3D()
+            {
+                x = y = z = 0.0;
+            }
+
+            // Конструктор з параметрами
+            public Vector3D(double x, double y, double z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+
+            // Метод для обчислення довжини вектора
+            public double Length()
+            {
+                return Math.Sqrt(x * x + y * y + z * z);
+            }
+
+            // Метод для обчислення скалярного добутку двох векторів
+            public static double DotProduct(Vector3D v1, Vector3D v2)
+            {
+                return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
+            }
+
+            // Метод для знаходження координат колінеарного вектора
+            public Vector3D Collinear(double k)
+            {
+                return new Vector3D(k * x, k * y, k * z);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x1 = double.Parse(textBox1.Text);
+            double y1 = double.Parse(textBox2.Text);
+            double z1 = double.Parse(textBox3.Text);
+
+            double x2 = double.Parse(textBox4.Text);
+            double y2 = double.Parse(textBox5.Text);
+            double z2 = double.Parse(textBox6.Text);
+
+            double k = double.Parse(textBox10.Text);
+
+            // Створення об'єктів векторів
+            Vector3D vector1 = new Vector3D(x1, y1, z1);
+            Vector3D vector2 = new Vector3D(x2, y2, z2);
+
+            // Обчислення довжини вектора
+            double length1 = vector1.Length();
+            textBox7.Text = length1.ToString();
+
+            // Обчислення скалярного добутку
+            double dotProduct = Vector3D.DotProduct(vector1, vector2);
+            textBox8.Text = dotProduct.ToString();
+
+            // Обчислення колінеарного вектора
+            Vector3D collinearVector = vector1.Collinear(k);
+            textBox9.Text = $"({collinearVector.X}, {collinearVector.Y}, {collinearVector.Z})";
+        }
+    }
+}
---
+++ laba2-27/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("laba2-27")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("laba2-27")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7f12c1e3-8dab-47e8-85c4-5a1f612dee30")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
